53830fb4aeb5f250e8a418174c6de1c467401508
Atanu pal <atanupal@live.com>
2025-02-14 02:31:50 +0530

    fix cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(
   cors({
-    // origin: ["http://127.0.0.1:3000", "http://localhost:3000"], // frontend URL
+    origin: ["http://127.0.0.1:5000", "http://localhost:5000"], // frontend URL
     credentials: true, // Allow credentials (cookies) to be sent
   })
 );
