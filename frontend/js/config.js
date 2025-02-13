5ebe632bb47679d040e8dc80e2c24c54b38acb5f
Atanu pal <atanupal@live.com>
2025-02-14 03:18:28 +0530

    fix url

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,6 +1,6 @@
 const config = {
   apiBaseUrl:
-    "http://taskmanager-env.eba-vqjyw7u2.ap-south-1.elasticbeanstalk.com",
+    "http://taskmanager-env.eba-vqjyw7u2.ap-south-1.elasticbeanstalk.com/",
   // "http://localhost:5000/",
 };
 
