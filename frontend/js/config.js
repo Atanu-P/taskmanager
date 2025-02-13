c5f3c3d7185a4b2425865415d0e1ee0d0fa4da08
Atanu pal <atanupal@live.com>
2025-02-14 03:13:21 +0530

    update api base url

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,5 +1,7 @@
 const config = {
-  apiBaseUrl: "http://localhost:5000/",
+  apiBaseUrl:
+    "http://taskmanager-env.eba-vqjyw7u2.ap-south-1.elasticbeanstalk.com",
+  // "http://localhost:5000/",
 };
 
 export default config;
