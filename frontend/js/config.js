c879303ab57221e798ae67fe3a5885a834948049
Atanu pal <atanupal@live.com>
2025-02-16 02:54:03 +0530

    remove workflow file

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,7 +1,5 @@
 const config = {
-  apiBaseUrl:
-    "http://taskmanager-env.eba-vqjyw7u2.ap-south-1.elasticbeanstalk.com/",
-  // "http://localhost:5000/",
+  apiBaseUrl: "http://localhost:5000/",
 };
 
 export default config;
